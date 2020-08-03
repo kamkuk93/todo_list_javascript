708abac4bb757f1a646709d4daa8e1db16502f83
Kamil <kamkuk93@gmail.com>
2020-08-03 17:37:20 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,158 @@
+let $todoInput;  // mirjdcr, gdxir uźytkoenik wpisuje treść zadania
+let $alertInfo;  // info o braku zadań / konieczności dodania tekstu
+let $addBtn;  //przycisk ADD - dodaje nowe elementy do listy
+let $ulList;  // nasza lista zadań, tagi <ul></ul>
+let $newTask;  //nowo dodany LI, nowe zadanie
+
+let $popup;  //pobrany popup
+let $popupInfo;  //alert w popupie, jak się doda pusty tekst
+let $editedTodo;  //edytowany Todo
+let $popupInput;  //tekst wpisywany w inputa w popupie
+let $addPopupBtn;  //przycisk "zatwierdź" w popupie
+let $closeTodoBtn;  //przycisk do zamykania popup`a
+
+let $idNumber = 0;
+let $allTasks;
+
+
+const main = () => {
+    prepareDOMElements();
+    prepareDOMEvents();
+};
+
+
+// pobierane elementy
+const prepareDOMElements = () => {
+    $todoInput = document.querySelector('.todoInput');
+    $alertInfo = document.querySelector('.alertInfo');
+    $addBtn = document.querySelector('.addBtn');
+    $ulList = document.querySelector('.todoList ul');
+
+    $popup = document.querySelector('.popup');
+    $popupInfo = document.querySelector('.popupInfo');
+    $popupInput = document.querySelector('.popupInput');
+    $addPopupBtn = document.querySelector('.accept');
+    $closeTodoBtn = document.querySelector('.cancel');
+    $allTasks = $ulList.getElementsByTagName('li');
+};
+
+
+
+
+// nadajemy nasłuchiwanie
+const prepareDOMEvents = () => {
+
+    $addBtn.addEventListener('click', addNewTask);
+    $ulList.addEventListener('click', checkClick);
+    $closeTodoBtn.addEventListener('click', closePopup);
+    $addPopupBtn.addEventListener('click', changeTodo);
+    $todoInput.addEventListener('keyup', enterCheck);
+};
+
+
+
+// dodajemy nowy element do listy
+const addNewTask = () => {
+    if ($todoInput.value !== '') {
+        $idNumber++;
+        $newTask = document.createElement('li');
+        $newTask.innerText = $todoInput.value;
+        $newTask.setAttribute('id', `todo-${$idNumber}`);
+        $ulList.appendChild($newTask);
+        $todoInput.value = '';
+        $alertInfo.innerText = '';
+        createToolsArea();
+    } else {
+        $alertInfo.innerText = 'Wpisz treść zadania';
+    }
+};
+
+const enterCheck = () => {
+    if (event.keyCode === 13) {
+        addNewTask();
+    }
+};
+
+// tworzymy przyciski edycji, usuwania i "gotowe"
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div');
+    toolsPanel.classList.add('tools');
+    $newTask.appendChild(toolsPanel);
+
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fas fa-check" </i>';
+
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.innerText = 'EDIT';
+
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times" </i>';
+
+    toolsPanel.appendChild(completeBtn);
+    toolsPanel.appendChild(editBtn);
+    toolsPanel.appendChild(deleteBtn);
+
+};
+
+
+// zarządzanie kliknięciami w przyciski
+const checkClick = (e) => {
+    if (e.target.closest('button').classList.contains('complete')) {
+
+        e.target.closest('li').classList.toggle('completed');
+        e.target.closest('button').classList.toggle('completed');
+
+    } else if (e.target.closest('button').className === 'edit') {
+        console.log('edit');
+
+        editTask(e);
+
+    } else if (e.target.closest('button').className === 'delete') {
+        deleteTask(e);
+    }
+};
+
+// edycja zadania
+const editTask = (e) => {
+    const oldTodo = e.target.closest('li').id;
+    $editedTodo = document.getElementById(oldTodo);
+    $popupInput.value = $editedTodo.firstChild.textContent;
+    $popup.style.display = 'flex';
+}
+
+// sprawdzamy czy popup nie jest pusty i zmieniamy treść zadania
+const changeTodo = () => {
+    if ($popupInput.value !== '') {
+        $editedTodo.firstChild.textContent = $popupInput.value;
+        $popup.style.display = 'none';
+        $popupInfo.innerText = '';
+    } else {
+        $popupInfo.innerText = 'Musisz podać jakąś treść!';
+    }
+};
+
+
+
+//zamykanie popupa
+const closePopup = () => {
+    $popup.style.display = 'none';
+    $popupInfo.innerText = '';
+};
+
+// usuwanie zadania
+const deleteTask = (e) => {
+    const deleteTodo = e.target.closest('li');
+    deleteTodo.remove();
+
+    if ($allTasks.length === 0) {
+        $alertInfo.innerText = 'Brak zadań na liście.';
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', main);
